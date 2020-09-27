3520b03afff101c25b5fdd5a7fdd43a596623574
Conor Gilsenan <conorgilsenan@gmail.com>
2020-09-26 22:05:54 -0400

    Add drop box page from iwillvote.com

---
+++ assets/locate.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const srcTemplate = "https://iwillvote.com/locate/results?address=";
+  const mapElement = document.querySelector("#iwillvote");
+  const addressElement = document.querySelector("#address");
+  const buttonElement = document.querySelector("#btn");
+  const addressTextElement = document.querySelector("#addressText");
+
+  buttonElement.onclick = function () {
+    console.log("WOO!");
+
+    // Populate address text.
+    const address = addressElement.value;
+    addressTextElement.textContent = address;
+
+    // Update map.
+    const encodedAddress = encodeURIComponent(address);
+    const newSrc = srcTemplate + encodedAddress;
+    mapElement.src = newSrc;
+
+    // Display results.
+    mapElement.removeAttribute("style");
+    addressTextElement.parentElement.removeAttribute("style");
+  };
+});
