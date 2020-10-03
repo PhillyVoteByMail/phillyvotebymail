c839a4b239fed2ccd40456cf56e2c86e9878344b
kat3l1bby <kat3l1bby@protonmail.com>
2020-10-02 21:21:22 -0400

    v2_theme

--- v2/index.php
+++ v2/index.php
@@ -1,17 +1,63 @@
 <?php
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
+ * The main template file
  *
- * @package WordPress
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link    https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package zakra
  */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+get_header();
+?>
 
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+	<div id="primary" class="content-area">
+		<?php echo apply_filters( 'zakra_after_primary_start_filter', false ); // WPCS: XSS OK. ?>
+
+		<?php
+		if ( have_posts() ) :
+
+			if ( is_home() && ! is_front_page() ) :
+				if ( 'page-header' !== zakra_is_page_title_enabled() ) :
+					?>
+					<header class="page-header">
+						<h1 class="page-title tg-page-content__title"><?php single_post_title(); ?></h1>
+					</header><!-- .page-header -->
+				<?php
+				endif;
+			endif;
+
+			do_action( 'zakra_before_posts_the_loop' );
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				/*
+				 * Include the Post-Type-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_type() );
+
+			endwhile;
+
+			do_action( 'zakra_after_posts_the_loop' );
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+		<?php echo apply_filters( 'zakra_after_primary_end_filter', false ); // // WPCS: XSS OK. ?>
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
